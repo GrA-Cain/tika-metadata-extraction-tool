a5d901d941d7d99efb497eb358d02d0936f81ab8
marcovenema <m.venema@groningerarchieven.nl>
2026-05-19 09:35:06 +0200

    pipeline edits, basic ui start

--- src/DataFrame_class_pipeline.py
+++ src/DataFrame_class_pipeline.py
@@ -5,10 +5,11 @@ import openpyxl
 import xlsxwriter
 from itertools import combinations, chain
 from functools import cached_property
+import dtale
 
 
 class DataFrame_pipeline():
-    NAMESPACE_DICT = {"Message": "Message:|Message-",
+    NAMESPACE_DICT = {"Message ns": "Message:|Message-",
                 "Mapi" : "mapi:",
                 "dc" : "^dc:|dcterms",
                 "pdf" : "^pdf:|pdfaid|pdfa:|pdfuaid",
@@ -19,22 +20,26 @@ class DataFrame_pipeline():
                 "MSIP" : "^(?!pdf:).*MSIP_Label",
                 "Exif": "Exif|exif",
                 "Image" : "Image"
-                } #you can add namespaces here
+                } #you can add namespaces here using the following format name (key) : regex str(value). 
     
-
-
-    def __init__(self, root_dir : str | Path, log_folder_path : str | Path, xlsx_path : str | Path,
-                 output_DataFrames, output_options):
+    def __init__(self, root_dir : str | Path, log_folder_path : str | Path, xlsx_path : str | Path):
         self.root_dir = root_dir
         self.log_folder_path = log_folder_path
         self.xlsx_path = xlsx_path
-        self.output_options = ["df_raw", "df_percentage", "df_leftover_fields"] + self.filetype_list + list(self.NAMESPACE_DICT.keys())
-        self.options = ["visualiseren duplicaten", "add summary columns"]
+        self.output_options = ["basis", "Coverage (non-NA) of metadata by filetype", "Leftover fields"] + self.filetype_list + list(self.NAMESPACE_DICT.keys())
+        self.function_calls = [lambda: self.df_raw,
+                               lambda summary: self.field_coverage_by_filetype_df(summary = summary),
+                               lambda summary: self.create_dataframe_leftover_fields(summary = summary)]
+        self.function_calls.extend(lambda summary, ft = filetype: self.return_filetype_dfs(filetype = ft, summary = summary) for filetype in self.filetype_list)
+        self.function_calls.extend(lambda summary, ns = namespace: self.return_namespace_dfs(namespace = ns, summary= summary) for namespace in self.NAMESPACE_DICT.keys())
+        self.additional_options = ["Duplicate analysis", "Add summary columns"]
+        self.output_dataframe_dict = {key: value for key, value in zip(self.output_options, self.function_calls)}
 
     @cached_property
     def df_raw(self):
        return pd.DataFrame(data = ms(self.root_dir, self.log_folder_path))
 
+    @cached_property
     def filetype_list(self): 
         return list(self.df_raw["tika:file_ext"].unique())
     
@@ -58,11 +63,11 @@ class DataFrame_pipeline():
         for regex_str in self.filetype_list:
             df = self.df_raw
             df = df.set_index("tika:file_ext")
-            df = df.filetype(axis=0, regex=regex_str) 
+            df = df.filter(axis=0, regex=regex_str) 
             df = df.reset_index()
             df_index = df.replace("", None).notna().sum(axis = 1).sort_values(ascending = False).index
-            filetype_dict[regex_str] = df.reindex(df_index)
-            return filetype_dict
+            filetype_dict[regex_str] = df.reindex(df_index) ##ff indexen op bestandsnaam!
+        return filetype_dict
     
     def return_filetype_dfs(self, filetype : str, summary = False):
          return self.summary_columns(self.genereren_filetype_dfs[filetype]) if summary else self.genereren_filetype_dfs[filetype]
@@ -71,16 +76,17 @@ class DataFrame_pipeline():
     def genereren_namespace_dfs(self):
         namespace_return = {}
         for name, regex_str in DataFrame_pipeline.NAMESPACE_DICT.items():
-            df = self.df_raw.filetype(axis = 1, regex = regex_str).replace("", None).dropna(how = "all", axis = 0) 
+            df = self.df_raw.filter(axis = 1, regex = regex_str).replace("", None).dropna(how = "all", axis = 0) 
             df = df[df.isna().sum().sort_values(ascending=True).index] 
             column = self.df_raw.loc[df.index, "tika:file_ext"] 
             df = pd.concat([df, column], axis = 1) 
             df = df.set_index("tika:file_ext") 
             namespace_return[name] = df
+        #making image namespace (all metadata (excluding exif) that are found in jpg files but not in any other filetype)
         jpeg_only = self.df_raw.set_index("tika:file_ext").index.str.lower().str.match("jpg") 
         non_jpeg = ~jpeg_only 
         has_value_in_jpg = self.df_raw[jpeg_only].notna().any(axis=0)
-        has_value_in_non_jpg = self.df_raw[non_jpeg].notna().any(axis=0) 
+        has_value_in_non_jpg = self.df_raw[non_jpeg].notna().any(axis=0)
         jpg_only_cols = has_value_in_jpg & ~has_value_in_non_jpg 
         df = self.df_raw.loc[:, jpg_only_cols].dropna(how = "all", axis = 0).drop(namespace_return["Exif"].columns, axis = 1) 
         file_ext_column = self.df_raw.loc[df.index, "tika:file_ext"]
@@ -89,7 +95,7 @@ class DataFrame_pipeline():
         return namespace_return
     
     def return_namespace_dfs(self, namespace : str, summary = False):
-       return self.summary_columns(self.namespace_return[namespace]) if summary else self.genereren_namespace_dfs[namespace]
+       return self.summary_columns(self.genereren_namespace_dfs[namespace]) if summary else self.genereren_namespace_dfs[namespace]
     
     #overbodig?
     def return_namespace_columns(self):
@@ -128,7 +134,7 @@ class DataFrame_pipeline():
         return combined
     
     @cached_property
-    def berekenen_percentages(self, summary = False):
+    def field_coverage_by_filetype_df(self, summary = False):
         percentage_dict = {}
         for filetype in self.filetype_list:
             filetype_df = self.df_raw.set_index("tika:file_ext").filter(axis = 0, regex = filetype)
@@ -140,41 +146,21 @@ class DataFrame_pipeline():
         df = pd.concat([df, df_mean_values], axis=1).rename({0 : 'gemiddelde'}, axis = 1).sort_values(axis = 0, by = ['gemiddelde'] , ascending= False)
         namespace_series = self.make_namespace_series()
         df = df.assign(namespace = namespace_series)
-        return df if summary else df #set up logging
+        return df if summary else df #niet heel elegant maar zo krijg je tenminste geen type-error als je hier summary invult, fixen met logging?
     
-    def df_browser_viewer(self):
-        
-    def df_excel_writer(self):
-        excelwriter_options = ["df_raw", "df_percentage", "df_leftover_fields"]
-        options = ["visualiseren duplicaten", "summary_columns"]
-        excelwriter_options.extend(self.filetype_list)
-        excelwriter_options.extend(self.NAMESPACE_DICT.keys())
-        self.df_raw.to_excel(self.xlsx_path)
+    # def df_browser_viewer(self):
+    # dtale.show(df)
+    
+    #def output_duplicate_analysis
+       #viable choices list
+       # request_list
+    
+    def df_excel_writer(self, request_list: list, summary = False):
+        self.df_raw.sample(3000).to_excel(self.xlsx_path)
         with pd.ExcelWriter(self.xlsx_path, engine = "openpyxl", mode = "a") as writer:
-            
+            for request in request_list:
+               self.output_dataframe_dict.get(request)(summary = summary).to_excel(writer, sheet_name = request)
 
-# df_raw.to_excel("C:/Users/m.venema/Desktop/test/dataframe_test.xlsx")
-# with pd.ExcelWriter("C:/Users/m.venema/Desktop/test/dataframe_test.xlsx", engine = "openpyxl", mode = "a") as writer:
-#     df_msg_selectie.to_excel(writer, sheet_name = "selectie_msg")
-#     df_pdf_selectie.to_excel(writer, sheet_name = "pdf_selectie")
-#     df_docx_selectie.to_excel(writer, sheet_name = "docx_selectie")
-#     df_percentage.to_excel(writer, sheet_name = f"%bestandstype")
-#     df_distributie_T.to_excel(writer, sheet_name = "distributie van namespaces")
-#     df_docx.to_excel(writer, sheet_name = "docx")
-#     df_msg.to_excel(writer, sheet_name = "msg")
-#     df_pdf.to_excel(writer, sheet_name = "pdf")
-#     df_jpg.to_excel(writer, sheet_name = "jpg")
-#     df_xlsx.to_excel(writer, sheet_name = "xlsx")
-#     df_message_ns.to_excel(writer, sheet_name = "message ns")
-#     df_Mapi_ns.to_excel(writer, sheet_name = "mapi ns")
-#     df_meta_ns.to_excel(writer, sheet_name = "meta ns")
-#     df_dc_ns.to_excel(writer, sheet_name= "dc ns")
-#     df_ExtendedProperties_ns.to_excel(writer, sheet_name= "extentedproperties ns")
-#     df_xmp_ns.to_excel(writer, sheet_name= "xmp ns")
-#     df_exif_ns.to_excel(writer, sheet_name = "exif ns")
-#     df_pdf_ns.to_excel(writer, sheet_name= "pdf ns")
-#     df_Xtika_ns.to_excel(writer, sheet_name= "X-tika ns")
-#     df_MSIP_ns.to_excel(writer, sheet_name = "MSIP ns")
-#     df.to_excel(writer, sheet_name = "image ns")
-#     df_restant.to_excel(writer, sheet_name = "overige kolommen")
-#     df_raw.to_excel(writer, sheet_name = "volledig dataframe")
+
+
+#def df_browser_viewer(self):
