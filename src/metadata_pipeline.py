bd43ed3cacd05c249a44845cfcbf227874364522
Marco Venema <marcovenema94@gmail.com>
2026-05-20 13:06:03 +0200

    modified metadata pipeline

--- src/metadata_pipeline.py
+++ src/metadata_pipeline.py
@@ -7,154 +7,140 @@ import yaml
 
 
 class metadata_pipeline():
-   
-   def __init__(self, log_folder_path, root_dir):
-       self.log_folder_path = log_folder_path
-       self.root_dir = root_dir
-       self.json_logger = self.make_logger(name = "remove_json")
-       self.tika_logger = self.make_logger(name = "tika_metadata")
-       self.selectie_logger = self.make_logger(name = "metadata_selectie")
-       self.dataframe_logger = self.make_logger(name = "DataFrame_ingest")
-       
-
-   def make_logger(self, name: str):
-    logger = logging.getLogger(name)
-    self.log_folder_path = Path(self.log_folder_path)
-    if not logger.handlers:
-        handler = logging.FileHandler(filename = self.log_folder_path / f"{name}.log")
-        handler.setFormatter(logging.Formatter(("%(asctime)s - %(levelname)s - %(message)s")))
-        logger.setLevel(logging.DEBUG)
-        logger.addHandler(handler)
-    return logger
-
-
-##Selectie metadatavelden
-#zie metadata_toframe.ipynb voor context
-docx_metadata = {
-#technical"
-    "document_id": None, #populate at ingest
+    
+    DOCX_METADATA_STRUCTUUR = {
+    "document_id": None,
     "filename" : "resourceName",
     "path" : "tika_batch_fs:relative_path",
     "format" : "tika:file_ext",
-    "Media type" : "Content-Type", #recente term voor MIME-type
-    "ingested_at" : None, #populate at ingest
-    "origin": None, #source location, system, URL? Departement, organisatie?
-#descriptive"
+    "Media type" : "Content-Type",
+    "ingested_at" : None, 
+    "origin": None,
     "title": "dc:title",
     "creator" : "dc:creator", 
     "create date" : "dcterms:created",
-    "summary" : None, #fill with summerization output
-#structural"
+    "summary" : None, 
     "page-count": "meta:page-count",
     "word-count": "meta:word-count",
-    "Template" : "extended-properties:Template" #alleen wanneer dit veld structureel gevuld is en een betekenisvolle naam heeft 
-}
+    "Template" : "extended-properties:Template"}
     
-
-pdf_metadata = {
-#technical"
-    "document_id": None, #populate at ingest
+    PDF_METADATA_STRUCTUUR = {
+     "document_id": None, 
     "filename" : "resourceName",
     "path" : "tika_batch_fs:relative_path",
     "format" : "tika:file_ext",
-    "Media type" : "Content-Type", #recente term voor MIME-type
+    "Media type" : "Content-Type", 
     "Producer software" : "pdf:producer",
-    "ingested_at" : None, #populate at ingest
-    "origin": None, #source location, system, URL? Departement, organisatie?
-#descriptive"
+    "ingested_at" : None, 
+    "origin": None, 
     "title": "dc:title",
     "language" : "dc:language",
-    "description" : "dc:description", #laag gevuld, misschien weghalen
+    "description" : "dc:description", 
     "creator" : "dc:creator", 
     "create date" : "dcterms:created",
-    "summary" : None, #fill with summerization output
-#structural"
+    "summary" : None, 
     "page-count": "xmpTPg:NPages",
     "Character per page" : "pdf:charsPerPage" ,
     "OCR page count" : "pdf:ocrPageCount",
-    "creator tool" : "xmp:CreatorTool", #low signal?
-    "Template" : "extended-properties:Template"
-}
+    "creator tool" : "xmp:CreatorTool", 
+    "Template" : "extended-properties:Template"}
 
-msg_metadata = {
-#technical
-        "document_id": None, #omitten en vervangen door message-id?
-        "filename": "resourceName",
-        "path": "tika_batch_fs:relative_path",
-        "format": "tika:file_ext",
-        "media_type": "Content-Type",
-        "ingested_at": None, #populate at ingest
-        "origin": None, #source location, system, URL? Departement, organisatie?
-#descriptive
-        "subject": "dc:title", #dc:subject geeft dezelfde waarden m.u.v het missen van "RE:" op reply emails
-        "sender": "Message:From", #Message:Raw-Header:From -> voor naam + emailadres
-        "sender email": "Message:From-Email",
-        "recipients_to": "Message-To",
-        "recipients_cc": "Message-Cc",
-        "recipient email" : "Message-Recipient-Address",
-        "sent_date" : "Message:Raw-Header:Date",
-        "received_date": "mapi:message-delivery-time",
-        "summary": None, #fill with summerization output #deze nodig voor emails?
-#structural"
-        "has_attachments": "Message:Raw-Header:X-MS-Has-Attach", #BOOL
-        "body_format": "mapi:body-types-processed",  # plain text of HTML body
-        "reply_to": "Message:Raw-Header:In-Reply-To",  #ID van email 
-        "message_id": "Message:Raw-Header:Message-ID",   # for thread reconstruction
-}
+    MSG_METADATA_STRUCTUUR = {
+    "document_id": None,
+    "filename": "resourceName",
+    "path": "tika_batch_fs:relative_path",
+    "format": "tika:file_ext",
+    "media_type": "Content-Type",
+    "ingested_at": None, 
+    "origin": None, 
+    "subject": "dc:title", 
+    "sender": "Message:From", 
+    "sender email": "Message:From-Email",
+    "recipients_to": "Message-To",
+    "recipients_cc": "Message-Cc",
+    "recipient email" : "Message-Recipient-Address",
+    "sent_date" : "Message:Raw-Header:Date",
+    "received_date": "mapi:message-delivery-time",
+    "summary": None, 
+    "has_attachments": "Message:Raw-Header:X-MS-Has-Attach", 
+    "body_format": "mapi:body-types-processed",  
+    "reply_to": "Message:Raw-Header:In-Reply-To", 
+    "message_id": "Message:Raw-Header:Message-ID",}
+   
+    def __init__(self, log_folder_path, root_dir):
+        self.log_folder_path = Path(log_folder_path)
+        self.root_dir = Path(root_dir)
+        self.json_logger = self.make_logger(name = "Remove_json")
+        self.tika_logger = self.make_logger(name = "Tika_metadata")
+        self.selectie_logger = self.make_logger(name = "Metadata_selectie")
+        self.dataframe_logger = self.make_logger(name = "DataFrame_ingest")
+        self.tika_location = Path(__file__).parent
+       
 
-#note: de invoer van paden met '/' ipv '\' en als string 
-def remove_json(root_dir: str | Path, log_folder_path: str | Path, alle_json_verwijderen = False, yaml_verwijderen = False):
-        root_dir = Path(root_dir)
-        logger = make_logger("remove_json", log_folder_path)
-        files_verwijderd = 0 
-        if alle_json_verwijderen:
-            files = list(root_dir.rglob('*.json'))
-            type = "json"
-            logger.info(f"{len(files)} {type} files found")
-        else: 
-            files = list(root_dir.rglob('*.metadata.json'))
-            type = "metadata.json"
-            logger.info(f"{len(files)} {type} files found")
-        if yaml_verwijderen:
-            files = list(root_dir.rglob('*.metadata.yaml'))
-            type = "metadata.yaml"
-            logger.info(f"{len(files)} {type} files found")
-        for file in files:
+
+    def make_logger(self, name: str):
+        logger = logging.getLogger(name)
+        if not logger.handlers:
+            handler = logging.FileHandler(filename = self.log_folder_path / f"{name}.log")
+            handler.setFormatter(logging.Formatter(("%(asctime)s - %(levelname)s - %(message)s")))
+            logger.setLevel(logging.DEBUG)
+            logger.addHandler(handler)
+        return logger
+
+    def remove_json(self, alle_json_verwijderen = False, yaml_verwijderen = False):
+            logger = self.json_logger
+            files_verwijderd = 0 
+            files_list = []
+            if alle_json_verwijderen:
+                json_files = list(self.root_dir.rglob('*.json'))
+                json_type = "json"
+                logger.info(f"{len(json_files)} {json_type} files found")
+                files_list += json_files
+            else: 
+                json_metadata_files = list(self.root_dir.rglob('*.metadata.json'))
+                json_metadata_type = "metadata.json"
+                logger.info(f"{len(json_metadata_files)} {json_metadata_type} files found")
+                files_list += json_metadata_files
+            if yaml_verwijderen:
+                yaml_files = list(self.root_dir.rglob('*.metadata.yaml'))
+                yaml_type = "metadata.yaml"
+                logger.info(f"{len(yaml_files)} {yaml_type} files found")
+                files_list += yaml_files
+            for file in files_list:
+                try:
+                   file.unlink()
+                   files_verwijderd += 1
+                except Exception as e:
+                    logger.error(f"Something went wrong with {file}: {e}", exc_info = True)
+            logger.info(f"{files_verwijderd} files verwijderd")
+            if files_verwijderd == 0:
+                logger.info("did not delete any files")
+
+    def metadata_genereren(self, output_dir = None): ##working directory aanpassen
+        logger = self.tika_logger
+        all_paths = list(self.root_dir.rglob('*'))
+        logger.info(f"Scan: {len(all_paths)} paden gevonden! (mappen en bestanden)")
+        folders_list = []
+        files_list = []
+        for folders in all_paths: 
+            if folders.is_dir():
+                folders_list.append(folders)
+            elif folders.is_file():
+                files_list.append(folders)
+        logger.info(f"Scan: {len(folders_list)} folders found!")
+        logger.info(f"Scan: {len(files_list)} files found!")
+        files_list = list(set(files_list))
+        logger.info(f"Scan: {len(files_list)} files left after removing duplicates")
+        for index, paths in enumerate(folders_list): 
             try:
-               file.unlink()
-               files_verwijderd += 1
+                    cmd_command = f'call configure.bat && java -jar tika.jar -i "{paths}" -o "{output_dir if output_dir is not None else paths}" -J -excludeFilePat ".json"' 
+                    subprocess.run(cmd_command, shell = True, capture_output=True, timeout= 60, check=True, text = True, cwd= self.tika_location)
             except Exception as e:
-                logger.error(f"Something went wrong with {file}: {e}", exc_info = True)
-        logger.info(f"{files_verwijderd} {type} files verwijderd")
-        if files_verwijderd == 0:
-            logger.info("did not delete any files")
-
-#if java is installed,
-def metadata_genereren(root_dir: str | Path, logging_folder_path: str | Path): ##working directory aanpassen
-    root_dir = Path(root_dir)
-    logger = make_logger("metadata_genereren", logging_folder_path)
-    all_paths = list(root_dir.rglob('*'))
-    logger.info(f"Scan: {len(all_paths)} paden gevonden! (mappen en bestanden)")
-    folders_list = []
-    files_list = []
-    for folders in all_paths: #loop door alle paden, mapjes afvangen
-        if folders.is_dir():
-            folders_list.append(folders)
-        elif folders.is_file():
-            files_list.append(folders)
-    logger.info(f"Scan: {len(folders_list)} folders found!")
-    logger.info(f"Scan: {len(files_list)} files found!")
-    files_list = list(set(files_list))
-    logger.info(f"Scan: {len(files_list)} files left after removing duplicates")
-    for index, paths in enumerate(folders_list): #tika werkt met folders, dus folders lijst als input voor de cmd call
-        try:
-            cmd_command = f'call configure.bat && java -jar tika.jar -i "{paths}" -o "{paths}" -J -excludeFilePat ".json"'  #output path optie!
-            subprocess.run(cmd_command, shell = True, capture_output=True, timeout= 60, check=True, text = True, cwd='C:/Users/m.venema') #tika in de cmd uitvoeren
-        except Exception as e:
-            stder_output = getattr(e, "stderr", None)
-            logger.error(f"Something went wrong with {paths}: {e}, stderr: {stder_output}", exc_info=True)
-        if index % 2 == 0: #set modulus number higher when when working with large datasets
-            logger.info(f"progress: {index}/{len(folders_list)} folders processed") 
+                stder_output = getattr(e, "stderr", None)
+                logger.error(f"Something went wrong with {paths}: {e}, stderr: {stder_output}", exc_info=True)
+            if index % 2 == 0: #modulus omhoog voor grotere datasets, anders spam je de log vol met progress meldingen!
+                logger.info(f"progress: {index}/{len(folders_list)} folders processed")
+           
             
 def metadata_selectie(root_dir: str | Path, log_folder_path : str | Path, output_as_yaml: False):
     logger = make_logger("metadata selectie", log_folder_path)
