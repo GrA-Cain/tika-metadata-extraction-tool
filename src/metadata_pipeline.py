118f91de3cf96c1c443572fcc95566dcd0fe024f
Marco Venema <marcovenema94@gmail.com>
2026-05-25 09:12:12 +0200

    finished metadata_pipeline

--- src/metadata_pipeline.py
+++ src/metadata_pipeline.py
@@ -8,6 +8,7 @@ import yaml
 
 class metadata_pipeline():
     
+    #output metadata dictionary structuur
     DOCX_METADATA_STRUCTUUR = {
     "document_id": None,
     "filename" : "resourceName",
@@ -25,7 +26,7 @@ class metadata_pipeline():
     "Template" : "extended-properties:Template"}
     
     PDF_METADATA_STRUCTUUR = {
-     "document_id": None, 
+    "document_id": None, 
     "filename" : "resourceName",
     "path" : "tika_batch_fs:relative_path",
     "format" : "tika:file_ext",
@@ -133,7 +134,7 @@ class metadata_pipeline():
         logger.info(f"Scan: {len(files_list)} files left after removing duplicates")
         for index, paths in enumerate(folders_list): 
             try:
-                    cmd_command = f'call configure.bat && java -jar tika.jar -i "{paths}" -o "{output_dir if output_dir is not None else paths}" -J -excludeFilePat ".json"' 
+                    cmd_command = f'java -jar tika.jar -i "{paths}" -o "{output_dir if output_dir is not None else paths}" -J -excludeFilePat ".json"' 
                     subprocess.run(cmd_command, shell = True, capture_output=True, timeout= 60, check=True, text = True, cwd= self.tika_location)
             except Exception as e:
                 stder_output = getattr(e, "stderr", None)
@@ -142,82 +143,74 @@ class metadata_pipeline():
                 logger.info(f"progress: {index}/{len(folders_list)} folders processed")
            
             
-def metadata_selectie(root_dir: str | Path, log_folder_path : str | Path, output_as_yaml: False):
-    logger = make_logger("metadata selectie", log_folder_path)
-    root_dir = Path(root_dir) #Path conversie
-    files = list(root_dir.rglob('*.json')) #List alle paden .json file
-    logger.info(f"found {len(files)} .json file!")
-    files_made = 0
-    if not files:
-        logger.warning("Geen json file gevonden in de map")
-    for index, file in enumerate(files): #loop door json om metadata selectie file aan te maken
-        with open(file, mode = 'r', encoding = 'UTF-8') as f:
-            if file.suffixes[-2] == ".metadata": #eventuele .metadata file overslaan
-                continue
-            if file.suffixes[-2] == ".docx":  #let op: negatief indexen op de suffixes lijst anders worden bestandsnamen met punten erin niet meegenomen
-                json_object = json.load(f)
-                new_dict = {k: json_object[0].get(v) for k, v in docx_metadata.items()} #dict aanmaken op basis van docx_metadata structuur en de tika metadata
-            elif file.suffixes[-2] == ".pdf":
-                json_object = json.load(f)
-                new_dict = {k: json_object[0].get(v) for k, v in pdf_metadata.items()}
-            elif file.suffixes[-2] == ".msg":
-                json_object = json.load(f)
-                new_dict = {k: json_object[0].get(v) for k, v in msg_metadata.items()}
-            else:
-                continue
-            try:
-               if output_as_yaml:
-                   sidecar_file_name_extension = file.stem + '.metadata.yaml'
-               else:
-                sidecar_file_name_extension = file.stem + '.metadata.json'
-               #logger.debug(f"file stem: {file.stem}")
-               file_match = [f for f in list(root_dir.rglob(f"{file.stem}*")) if f.is_file()]
-               if not file_match:
-                   logger.warning(f"geen match voor {file.stem} gevonden, bestand overgeslagen!")
-                   continue
-               output_directory = file_match[0].parent
-               #logger.debug(f"output directory: {output_directory}")
-               side_car_path = output_directory / sidecar_file_name_extension
-               #logger.debug(f"sidecar path: {side_car_path}")
-               with open(side_car_path, "w", encoding='utf-8') as path:
-                   if output_as_yaml:
-                       yaml.dump(new_dict, path, encoding = "utf-8", allow_unicode = True)
-                   else:
-                       json.dump(new_dict, path, indent=2, ensure_ascii=False)
-               files_made += 1
-            except Exception as e:
-               logger.error(f"error: {e}")
-        if index % 50 == 0:
-            logger.info(f"progress: {index} of {len(files)} file processed!")
-    logger.info(f"finished: {files_made} metadata file created!")
-
-#note: de invoer van paden met '/' ipv '\' en als string 
-def metadata_dataframe(root_dir: str | Path, log_folder_path : str | Path):
-    logger = make_logger("metadata_dataframe", log_folder_path)
-    root_dir = Path(root_dir)
-    if root_dir.exists() == False:
-        logger.error(f"{root_dir} does not exist!")
-    files = list(root_dir.rglob('*.json'))
-    if not files:
-        logger.error("did not find any json files") 
-    logger.info(f"found {len(files)} json files")
-    metadata = []
-    for file in files:
-        try:
+    def metadata_selectie(self, output_as_yaml = False):
+        logger = self.selectie_logger
+        files = list(self.root_dir.rglob('*.json')) #List alle paden .json file
+        logger.info(f"found {len(files)} .json file!")
+        files_made = 0
+        if not files:
+            logger.warning("Geen json file gevonden in de map")
+        for index, file in enumerate(files): 
             with open(file, mode = 'r', encoding = 'UTF-8') as f:
-                all_metadata = json.load(f)
-                # if not all_metadata:
-                #     logger.error(f"PATH: {file} does not exist, skipping")
-                primary_metadata = all_metadata[0]
-                metadata.append(primary_metadata)
-        except Exception as e:
-                logger.error(f"Something went wrong with {file}: {e}", exc_info = True)
-    return metadata
+                if file.suffixes[-2] == ".metadata": 
+                    continue
+                if file.suffixes[-2] == ".docx":  
+                    tika_json_file = json.load(f)
+                    new_dict = {k: tika_json_file[0].get(v) for k, v in self.DOCX_METADATA_STRUCTUUR.items()} 
+                elif file.suffixes[-2] == ".pdf":
+                    tika_json_file = json.load(f)
+                    new_dict = {k: tika_json_file[0].get(v) for k, v in self.PDF_METADATA_STRUCTUUR.items()}
+                elif file.suffixes[-2] == ".msg":
+                    tika_json_file = json.load(f)
+                    new_dict = {k: tika_json_file[0].get(v) for k, v in self.MSG_METADATA_STRUCTUUR.items()}
+                else:
+                    continue
+                try:
+                    if output_as_yaml:
+                        sidecar_file_name_extension = file.stem + '.metadata.yaml'
+                    else:
+                        sidecar_file_name_extension = file.stem + '.metadata.json'
+                    file_match = [match for match in list(self.root_dir.rglob(f"{file.stem}*")) if match.is_file()]
+                    if not file_match:
+                        logger.warning(f"geen match voor {file.stem} gevonden, bestand overgeslagen!")
+                        continue
+                    output_directory = file_match[0].parent
+                    side_car_path = output_directory / sidecar_file_name_extension
+                    with open(side_car_path, "w", encoding='utf-8') as path:
+                        if output_as_yaml:
+                            yaml.dump(new_dict, path, encoding = "utf-8", allow_unicode = True)
+                        else:
+                            json.dump(new_dict, path, indent=2, ensure_ascii=False)
+                    files_made += 1
+                except Exception as e:
+                    logger.error(f"error: {e}")
+                if index % 50 == 0:
+                    logger.info(f"progress: {index} of {len(files)} file processed!")
+        logger.info(f"finished: {files_made} metadata file created!")
 
-#generator ipv een list??? explore
+    def metadata_dataframe(self):
+        logger = self.dataframe_logger
+        if not self.root_dir.exists():
+            logger.error(f"{self.root_dir} does not exist!")
+            logger.info("aborting!")
+            return None
+        files = list(self.root_dir.rglob('*.json'))
+        if not files:
+            logger.error("did not find any json files")
+            logger.info("aborting!")
+            return None
+        logger.info(f"found {len(files)} json files")
+        metadata = []
+        for file in files:
+            try:
+                with open(file, mode = 'r', encoding = 'UTF-8') as f:
+                    all_metadata = json.load(f)
+                    if not all_metadata:
+                        logger.error(f"PATH: {file} does not exist or could not be loaded, skipping")
+                        continue
+                    primary_metadata = all_metadata[0]
+                    metadata.append(primary_metadata)
+            except Exception as e:
+                    logger.error(f"Something went wrong with {file}: {e}", exc_info = True)
+        return metadata
 
-
-def run_pipeline(root_dir):
-    metadata_genereren(root_dir)
-    metadata_selectie(root_dir)
-    metadata_dataframe(root_dir)
